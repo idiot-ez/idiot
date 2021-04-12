23d69903d3cf49087b4bee6518e645caed738cc1
Apoint <highhighlow@protonmail.com>
2021-04-11 20:04:27 -0700

    init commit

---
+++ IdiotBot/Program.cs
@@ -0,0 +1,57 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace IdiotBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient client;
+
+        public async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+
+            client.Log += LogAsync;
+            client.Ready += ReadyAsync;
+            client.MessageReceived += MessageReceivedAsync;
+
+            var token = File.ReadAllText("tok.txt").Replace("\r", "").Replace("\n", "");
+
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task LogAsync(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"{client.CurrentUser}");
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            if (message.Author.Id == client.CurrentUser.Id)
+                return;
+
+            if (message.Content == "!ping")
+                await message.Channel.SendMessageAsync("pong!");
+
+            if(message.Content == "!jimi")
+                await message.Channel.SendMessageAsync(":rolling_eyes:");
+        }
+    }
+}
